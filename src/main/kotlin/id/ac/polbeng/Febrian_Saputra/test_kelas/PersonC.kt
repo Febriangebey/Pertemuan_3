11559a0316e33b3cac87d4942314a88166aa6e24
Febriangebey <febriangebey@gmail.com>
2022-03-24 11:51:30 +0700

    Ini merupakan kode program PersonB untuk lebih ringkas lagi di property deklarasi di kotlin

---
+++ src/main/kotlin/id/ac/polbeng/Febrian_Saputra/test_kelas/PersonC.kt
@@ -0,0 +1,14 @@
+package id.ac.polbeng.Febrian_Saputra.test_kelas.RPL
+
+class PersonC (var firstName: String, var lastName: String, var age: Int){}
+
+fun main(){
+    val budi = PersonC("Budi", "Guanawan",21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+
+}
+
+
+
+
