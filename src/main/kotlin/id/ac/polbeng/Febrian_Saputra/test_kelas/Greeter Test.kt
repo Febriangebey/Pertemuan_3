c3057ed6f7bd6022a7bd8d37717c006095d8a6c7
Febriangebey <febriangebey@gmail.com>
2022-03-24 11:21:27 +0700

    Ini merupakan kode program Greeter Test  di kotlin

---
+++ src/main/kotlin/id/ac/polbeng/Febrian_Saputra/test_kelas/Greeter Test.kt
@@ -0,0 +1,13 @@
+package id.ac.polbeng.Febrian_Saputra.test_kelas.RPL
+
+class `Greteer Test` {
+}
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
+}
